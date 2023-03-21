6b8e462c8f20728dc81fabbef72d6dfaf00a30c3
SanjuManna <sanjumanna2201@gmail.com>
2023-03-21 17:02:30 +0530

    Commit

--- src/components/LocationDisplay.jsx
+++ src/components/LocationDisplay.jsx
@@ -3,6 +3,6 @@ import { useLocation } from "react-router-dom";
 
 const LocationDisplay = () => {
   const locattion = useLocation();
-  return <div data-testid="test-display">{locattion.pathname}</div>;
+  return <div data-testid="location-display">{locattion.pathname}</div>;
 };
 export default LocationDisplay;
